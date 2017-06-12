86d06aa068fcbea41480b0e2b465b87a926dac52
Aurelian Spodarec <aurelians@libero.it>
2017-06-12 02:20:07 +0200

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,3 +16,62 @@
 
 	}, false);
 	
+
+
+
+//////////////////////////////////////////////////
+//
+// Slider
+//  
+//////////////////////////////////////////////////
+var controls = document.querySelectorAll('.controls');
+for(var i=0; i<controls.length; i++){
+	controls[i].style.display = 'inline-block';
+}
+
+var slides = document.querySelectorAll('.slider-item');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,4000);
+
+function nextSlide(){
+	goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+	slides[currentSlide].className = 'slider-item';
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = 'slider-item showing';
+}
+
+
+
+
+//////////////////////////////////////////////////
+//
+// Modal
+//  
+//////////////////////////////////////////////////
+var btnModal = document.querySelector(".btn-modal");
+var modalOverlay = document.createElement("div");
+var modalBox = document.querySelector(".modal__box");
+var modalClose = document.querySelector(".modal__closeButton");
+
+modalOverlay.setAttribute("class", "modal-overlay");
+
+btnModal.addEventListener('click', function() {
+    document.body.appendChild(modalOverlay);
+    modalOverlay.appendChild(modalBox).style.display="block";
+});
+
+// modalOverlay.addEventListener('click', function() {
+//   this.parentNode.removeChild(this);
+// });
+
+modalClose.addEventListener('click', function() {
+  modalOverlay.parentNode.removeChild(modalOverlay);
+});
+
